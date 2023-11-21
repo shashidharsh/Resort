f2e2be730006d67d224107a25db6deef4236bd1b
shashidharsh <shashidharhunswadkar@gmail.com>
2023-11-22 01:50:11 +0530

    Implemented Disable Dark Mode or Night Mode.

--- app/src/main/java/com/ssh/resort/MainActivity.kt
+++ app/src/main/java/com/ssh/resort/MainActivity.kt
@@ -3,6 +3,7 @@ package com.ssh.resort
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Disable Dark Mode or Night Mode
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
 
         var cvAdmin = findViewById<CardView>(R.id.cvAdmin)
         cvAdmin.setOnClickListener{
