dd5feca6d956e674d76f060130fc8df4f136d87d
shashidharsh <shashidharhunswadkar@gmail.com>
2024-01-20 20:37:58 +0530

    Implemented Zoom Generated QR Code with Animation in Checkin.

--- app/src/main/java/com/ssh/resort/Checkin.kt
+++ app/src/main/java/com/ssh/resort/Checkin.kt
@@ -1,5 +1,9 @@
 package com.ssh.resort
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.annotation.SuppressLint
 import android.app.Activity
 import android.app.Dialog
@@ -7,6 +11,7 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.Color
+import android.graphics.RectF
 import android.net.ConnectivityManager
 import android.net.Uri
 import android.os.AsyncTask
@@ -15,10 +20,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.Window
+import android.view.animation.DecelerateInterpolator
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.RadioButton
@@ -93,6 +100,16 @@ class Checkin : AppCompatActivity() {
     var etCashAmount :EditText? = null
 
     var ivQrCode: ImageView? = null
+    var expandImage :ImageView? = null
+    var qrCodeImageBitmap: Bitmap? = null
+    var container: FrameLayout? = null
+
+    // Hold a reference to the current animator so that it can be canceled midway.
+    private var currentAnimator: Animator? = null
+
+    // The system "short" animation time duration in milliseconds. This duration is ideal for subtle
+    // animations or animations that occur frequently.
+    private var shortAnimationDuration: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -219,6 +236,8 @@ class Checkin : AppCompatActivity() {
         etNoOfPersonForActivity = findViewById(R.id.etNoOfPersonsActivities)
         etCashAmount = findViewById(R.id.etCashAmount)
         ivQrCode = findViewById(R.id.idIVQrcode)
+        expandImage = findViewById(R.id.expanded_image)
+        container = findViewById(R.id.container)
 
         //Calculate Data
         var calculate = findViewById<Button>(R.id.btnCalculate)
@@ -427,6 +446,15 @@ class Checkin : AppCompatActivity() {
                 }
             }
         }
+
+        // Hook up taps on the thumbnail views.
+        //Zoom QR Code
+        ivQrCode!!.setOnClickListener {
+            zoomImageFromThumb(it, qrCodeImageBitmap)
+        }
+
+        // Retrieve and cache the system's default "short" animation time.
+        shortAnimationDuration = resources.getInteger(android.R.integer.config_shortAnimTime)
     }
 
     private fun generateQR(amount: String?) {
@@ -441,8 +469,8 @@ class Checkin : AppCompatActivity() {
 //                "Q7lugo8mfJhDk6wIhANZkbXOWWR2lhJOH2Qs/OQRaRFD2oBuPCGtrMaVFR23t"
 
         try {
-            val bitmap = textToImageEncode(url)
-            ivQrCode!!.setImageBitmap(bitmap)
+            qrCodeImageBitmap = textToImageEncode(url)
+            ivQrCode!!.setImageBitmap(qrCodeImageBitmap)
             ivQrCode!!.invalidate()
         } catch (e: WriterException) {
             e.printStackTrace()
@@ -471,6 +499,132 @@ class Checkin : AppCompatActivity() {
         val bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444)
         bitmap.setPixels(pixels, 0, 400, 0, 0, 400, 400)
         return bitmap
+    }
+
+    private fun zoomImageFromThumb(thumbView: View, imageBitmap: Bitmap?) {
+        // If there's an animation in progress, cancel it immediately and
+        // proceed with this one.
+        currentAnimator?.cancel()
+
+        // Load the high-resolution "zoomed-in" image.
+        expandImage!!.setImageBitmap(imageBitmap)
+
+        // Calculate the starting and ending bounds for the zoomed-in image.
+        val startBoundsInt = android.graphics.Rect()
+        val finalBoundsInt = android.graphics.Rect()
+        val globalOffset = android.graphics.Point()
+
+        // The start bounds are the global visible rectangle of the thumbnail,
+        // and the final bounds are the global visible rectangle of the
+        // container view. Set the container view's offset as the origin for the
+        // bounds, since that's the origin for the positioning animation
+        // properties (X, Y).
+        thumbView.getGlobalVisibleRect(startBoundsInt)
+        container!!.getGlobalVisibleRect(finalBoundsInt, globalOffset)
+        startBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+        finalBoundsInt.offset(-globalOffset.x, -globalOffset.y)
+
+        val startBounds = RectF(startBoundsInt)
+        val finalBounds = RectF(finalBoundsInt)
+
+        // Using the "center crop" technique, adjust the start bounds to be the
+        // same aspect ratio as the final bounds. This prevents unwanted
+        // stretching during the animation. Calculate the start scaling factor.
+        // The end scaling factor is always 1.0.
+        val startScale: Float
+        if ((finalBounds.width() / finalBounds.height() > startBounds.width() / startBounds.height())) {
+            // Extend start bounds horizontally.
+            startScale = startBounds.height() / finalBounds.height()
+            val startWidth: Float = startScale * finalBounds.width()
+            val deltaWidth: Float = (startWidth - startBounds.width()) / 2
+            startBounds.left -= deltaWidth.toInt()
+            startBounds.right += deltaWidth.toInt()
+        } else {
+            // Extend start bounds vertically.
+            startScale = startBounds.width() / finalBounds.width()
+            val startHeight: Float = startScale * finalBounds.height()
+            val deltaHeight: Float = (startHeight - startBounds.height()) / 2f
+            startBounds.top -= deltaHeight.toInt()
+            startBounds.bottom += deltaHeight.toInt()
+        }
+
+        // Hide the thumbnail and show the zoomed-in view. When the animation
+        // begins, it positions the zoomed-in view in the place of the
+        // thumbnail.
+        thumbView.alpha = 0f
+
+        animateZoomToLargeImage(startBounds, finalBounds, startScale)
+
+        setDismissLargeImageAnimation(thumbView, startBounds, startScale)
+    }
+
+    private fun animateZoomToLargeImage(startBounds: RectF, finalBounds: RectF, startScale: Float) {
+        expandImage!!.visibility = View.VISIBLE
+
+        // Set the pivot point for SCALE_X and SCALE_Y transformations to the
+        // top-left corner of the zoomed-in view. The default is the center of
+        // the view.
+        expandImage!!.pivotX = 0f
+        expandImage!!.pivotY = 0f
+
+        // Construct and run the parallel animation of the four translation and
+        // scale properties: X, Y, SCALE_X, and SCALE_Y.
+        currentAnimator = AnimatorSet().apply {
+            play(
+                ObjectAnimator.ofFloat(expandImage, View.X, startBounds.left, finalBounds.left)).apply {
+                with(ObjectAnimator.ofFloat(expandImage, View.Y, startBounds.top, finalBounds.top))
+                with(ObjectAnimator.ofFloat(expandImage, View.SCALE_X, startScale, 1f))
+                with(ObjectAnimator.ofFloat(expandImage, View.SCALE_Y, startScale, 1f))
+            }
+            duration = shortAnimationDuration.toLong()
+            interpolator = DecelerateInterpolator()
+            addListener(object : AnimatorListenerAdapter() {
+
+                override fun onAnimationEnd(animation: Animator) {
+                    currentAnimator = null
+                }
+
+                override fun onAnimationCancel(animation: Animator) {
+                    currentAnimator = null
+                }
+            })
+            start()
+        }
+    }
+
+    private fun setDismissLargeImageAnimation(thumbView: View, startBounds: RectF, startScale: Float) {
+        // When the zoomed-in image is tapped, it zooms down to the original
+        // bounds and shows the thumbnail instead of the expanded image.
+        expandImage!!.setOnClickListener {
+            currentAnimator?.cancel()
+
+            // Animate the four positioning and sizing properties in parallel,
+            // back to their original values.
+            currentAnimator = AnimatorSet().apply {
+                play(ObjectAnimator.ofFloat(expandImage, View.X, startBounds.left)).apply {
+                    with(ObjectAnimator.ofFloat(expandImage, View.Y, startBounds.top))
+                    with(ObjectAnimator.ofFloat(expandImage, View.SCALE_X, startScale))
+                    with(ObjectAnimator.ofFloat(expandImage, View.SCALE_Y, startScale))
+                }
+                duration = shortAnimationDuration.toLong()
+                interpolator = DecelerateInterpolator()
+                addListener(object : AnimatorListenerAdapter() {
+
+                    override fun onAnimationEnd(animation: Animator) {
+                        thumbView.alpha = 1f
+                        expandImage!!.visibility = View.GONE
+                        currentAnimator = null
+                    }
+
+                    override fun onAnimationCancel(animation: Animator) {
+                        thumbView.alpha = 1f
+                        expandImage!!.visibility = View.GONE
+                        currentAnimator = null
+                    }
+                })
+                start()
+            }
+        }
     }
 
     //Download Agent Details from Server
